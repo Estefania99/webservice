7a3353dfe0c7e0f2843bc4a68029ec8eb1ee75b6
Estefania99 <1717110652@utectulancingo.edu.mx>
2019-03-31 13:25:41 -0600

    Correccion de errores

--- application/models/config.py
+++ application/models/config.py
@@ -3,8 +3,8 @@ import web
 print"conectando"
 db = web.database(
     dbn='mysql', # motor de base de datos
-    host='nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-    db='ewde06osbascmrfl'
+    host='nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    db='ewde06osbascmrfl',
     user='mp8c743cn6p4ufs2', # usuario dela base de datos
     pw='sb5h3oqh741d4bx6', # password del usuario
     port = 3306 # puerto de mariadb
