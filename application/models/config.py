5f27182b496651a14f670da6f043afe435280a6c
Alexis Hernandez Perez <1717110652@utectulancingo.edu.mx>
2019-03-30 21:17:48 -0600

    web service actualizado

--- application/models/config.py
+++ application/models/config.py
@@ -6,7 +6,7 @@ db = web.database(
     host='nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
     db='ewde06osbascmrfl'
     user='mp8c743cn6p4ufs2', # usuario dela base de datos
-    pw='xoqtqltxp8grrdqj', # password del usuario
+    pw='sb5h3oqh741d4bx6', # password del usuario
     port = 3306 # puerto de mariadb
     )
 print"conectado"
