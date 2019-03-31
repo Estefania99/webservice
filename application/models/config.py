d22ceec86aadd0ad49d31aae5f9688bdfc9fe8d7
Estefania99 <1717110652@utectulancingo.edu.mx>
2019-03-30 20:55:29 -0600

    Actualizacion de config para utilizarlo de forma remota

--- application/models/config.py
+++ application/models/config.py
@@ -3,10 +3,10 @@ import web
 print"conectando"
 db = web.database(
     dbn='mysql', # motor de base de datos
-    host='localhost', # ruta del servidor
-    db='ferreteria_egr', # nombre de la base de datos
-    user='egr5', # usuario dela base de datos
-    pw='egr5.2019', # password del usuario
-    port = 3308 # puerto de mariadb
+    host='nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+    db='ewde06osbascmrfl'
+    user='mp8c743cn6p4ufs2', # usuario dela base de datos
+    pw='xoqtqltxp8grrdqj', # password del usuario
+    port = 3306 # puerto de mariadb
     )
 print"conectado"
